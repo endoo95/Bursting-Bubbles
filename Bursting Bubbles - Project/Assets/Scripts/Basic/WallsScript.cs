9cbde2c3bde1b9de0b443cca5b18d4a0c3ddb578
Endoo-L <endoo95@gmail.com>
2022-07-09 13:03:17 +0200

    Prototype done

---
+++ Bursting Bubbles - Project/Assets/Scripts/Basic/WallsScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallsScript : MonoBehaviour
+{
+    private Vector2 _screenWidthBounds;
+
+    [SerializeField] private GameObject _rightWall;
+    [SerializeField] private GameObject _leftWall;
+    private void Awake()
+    {
+        _screenWidthBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height * 0.5f, Camera.main.transform.position.z));
+
+        _rightWall.transform.position = new Vector2(_screenWidthBounds.x, _screenWidthBounds.y);
+        _leftWall.transform.position = new Vector2(_screenWidthBounds.x * -1f, _screenWidthBounds.y);
+    }
+}
